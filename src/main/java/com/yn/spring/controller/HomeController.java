2b4a316c425042a8342bea6617041bbeebedd2e9
yangnan3 <yannan3@le.com>
2017-08-01 16:27:03 +0800

    修改bug

--- src/main/java/com/yn/spring/controller/HomeController.java
+++ src/main/java/com/yn/spring/controller/HomeController.java
@@ -1,22 +1,15 @@
 package com.yn.spring.controller;
 
-<<<<<<< HEAD
 import com.yn.service.UserService;
 import com.yn.spring.annotation.Form;
 import com.yn.spring.param.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-=======
-import com.yn.spring.service.UserService;
->>>>>>> c3d760a3ed890a056dfb5b4d66c806dd0ead7c3a
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-<<<<<<< HEAD
 import javax.annotation.PostConstruct;
-=======
->>>>>>> c3d760a3ed890a056dfb5b4d66c806dd0ead7c3a
 import javax.annotation.Resource;
 
 /**
@@ -26,23 +19,18 @@ import javax.annotation.Resource;
 @RequestMapping("home")
 public class HomeController extends BaseController {
 
-<<<<<<< HEAD
     private static Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Resource
     private UserService userService;
     private int count = 0;
-=======
-    @Resource
-    private UserService userService;
->>>>>>> c3d760a3ed890a056dfb5b4d66c806dd0ead7c3a
 
     @RequestMapping("/test")
     @ResponseBody
     public Object test() {
         try {
-            userService.add(1,2);
-            userService.print("哈哈哈");
+//            userService.add(1,2);
+//            userService.privateint("哈哈哈");
             Thread.sleep(1000);
         } catch (Exception e) {
             e.printStackTrace();
--- src/test/java/com/yn/test/TestLogId.java
+++ src/test/java/com/yn/test/TestLogId.java
@@ -10,6 +10,9 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -29,22 +32,31 @@ public class TestLogId {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        final CountDownLatch countDownLatch = new CountDownLatch(10);
+        final ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+
         ClassPathResource resource = new ClassPathResource("log4j.xml");
         System.out.println(resource.getFile().getAbsolutePath());
         DOMConfigurator.configure(resource.getFile().getAbsolutePath());
         MDC.put("test", String.valueOf(generator.generateKey()));
+        System.out.println("test:" + MDC.get("test"));
+
+
         for (int i=0; i<10; i++) {
-            new Thread(new Runnable() {
+            executorService.submit(new Runnable() {
                 @Override
                 public void run() {
                     MDC.put("test", String.valueOf(generator.generateKey()));
                     log(String.valueOf(count.addAndGet(1)));
                     log(String.valueOf(count.addAndGet(1)) + " 测试");
+                    threadLocal.set(count.get());
+                    countDownLatch.countDown();
                 }
-            }).start();
+            });
         }
 
-        Thread.sleep(10 * 1000);
+        countDownLatch.await();
 
         Map map = MDC.getCopyOfContextMap();
         System.out.println(map);
